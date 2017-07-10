03567b36f2e57e26e6f5337698d9ef3ff8b33158
Chris Houdlette <choudlette@gmail.com>
2017-07-09 21:51:27 -0600

    continuing the course

---
+++ src/docs/examples/ProgressBar/Example100Percent.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ProgressBar from 'ps-react/ProgressBar';
+
+
+/** 100% Example Height 20px */
+export default function Example100Percent() {
+  return <ProgressBar width={200} height={10} percent={100} />
+}
---
+++ src/docs/examples/ProgressBar/ExampleProgressBar.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ProgressBar from 'ps-react/ProgressBar';
+
+
+/** 80% Example */
+export default function ExampleProgressBar() {
+  return <ProgressBar width={200} percent={80} />
+}
